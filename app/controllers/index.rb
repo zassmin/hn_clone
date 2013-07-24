de2adb928fdf207d148d8e11e23b66a06ac04c27
DBC-Apprentice <dbc@dbc.com>
2013-07-24 12:28:36 -0700

    user can sign up

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,3 +7,15 @@ get '/post/:post_id' do
   @post = Post.find(params[:post_id])
   erb :show_post
 end
+
+get '/user/new' do
+  erb :new_user
+end
+
+post '/user/new' do
+  User.create(params[:user])
+end
+
+get '/user/:user_id' do
+
+end
